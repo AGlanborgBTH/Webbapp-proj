1ba33c865eb29ef75a7529c5a2597d64667042c3
AGlanborgBTH <93671007+AGlanborgBTH@users.noreply.github.com>
2022-05-18 00:37:15 +0200

    Created
    Created project

---
+++ App.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Base, Forms, Typography, Unique } from "./styles"
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ ...Base.base }}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+  );
+}
